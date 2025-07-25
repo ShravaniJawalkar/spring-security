deb79fe6f665f519080f7962faf43d9b5ccf9294
Shravani_Jawalkar <sjawalkar@corelogic.com>
2025-07-25 17:07:52 +0530

    explict password encoder defination

--- src/main/java/org/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/springsecurity/config/SecurityConfig.java
@@ -14,18 +14,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Using BCryptPasswordEncoder for password encoding
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.builder().username("dummyUser")
-                .password("{noop}dummyPassword") // {noop} indicates no password encoder
+                .password(new BCryptPasswordEncoder().encode("dummyPassword")) // {noop} indicates no password encoder
                 .roles("USER")
                 .build();
         //here we are providing id of encrypted password, in this case it is bcrypt, so internally it will use DelegatingPasswordEncoder,
         // and then it will delegate it to BCryptPasswordEncoder
         UserDetails user1 = User.builder().username("adminUser")
-                .password("{bcrypt}dummyAdminPassword")
+                .password(new BCryptPasswordEncoder().encode("adminPassword"))
                 .roles("ADMIN")
                 .build();// {bcrypt} indicates bcrypt password encoder
 
